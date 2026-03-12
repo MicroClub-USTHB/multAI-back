3589738b72c04798c364ac52163cbe2903bb0311
merakleee <mahammedabdallahslimane@gmail.com>
2026-03-12 02:08:56 +0100

    register filters in FilterFactory

--- app/service/AIService/data_processing/data_processor.py
+++ app/service/AIService/data_processing/data_processor.py
@@ -1,22 +1,27 @@
 from abc import ABC, abstractmethod
+from app.service.AIService.data_processing.filters.blur_filter import BlurFilter
+from app.service.AIService.data_processing.filters.brightness_filter import BrightnessFilter
+
 
 class BaseFilter(ABC):
 
     @abstractmethod
-    def verify_image(self, image)->bool:
+    def verify_image(self, image) -> bool:
         pass
 
     @abstractmethod
     def process_image(self, image):
         pass
 
+
 class FilterFactory:
     def __init__(self):
         self.filters = {
-            # blur_filter: BlurFilter
+            "blur_filter": BlurFilter,
+            "brightness_filter": BrightnessFilter,
         }
-    
-    def get_filter(self, filter_type:str)->BaseFilter:
+
+    def get_filter(self, filter_type: str) -> BaseFilter:
         if filter_type in self.filters:
             return self.filters[filter_type]
         else:
