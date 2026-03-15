11c377da6b92430ddd29f2c2f74faeb7c57406ea
khiro45 <kiro.thori@gmail.com>
2026-03-15 14:42:27 +0100

    feat : added vectore store functionalitys

--- app/service/AIService/vector_store/vector_store.py
+++ app/service/AIService/vector_store/vector_store.py
@@ -1,18 +1,45 @@
+from itertools import count
 
-
+from qdrant_client import QdrantClient
+from collections import Counter
 
 class VectorStore:
-    def __init__(self):
-        pass
-    
-    def init_vectore_store(self ):
-        pass
+    def __init__(self , vectore_size: int , collection_name: str):
+        self.vectore_size = vectore_size
+        self.client = self.init_vector_store(collection_name)
+        self.collection_name = collection_name
 
-    def get_vector_store(self):
-        pass
+    def init_vector_store(self, collection_name: str):
+        self.collection_name = collection_name
+        self.client.recreate_collection(
+            collection_name=self.collection_name,
+            vectors_config={
+                "size": self.vectore_size,
+                "distance": "Cosine"
+            }
+        )
     
-    def add(self, vector:list[float], metadata:dict):
-        pass
+    def add_embedding(self,id: str, vector:list[float], metadata:dict):
+        self.client.upsert(
+            collection_name=self.collection_name,
+            points=[
+                {
+                    "id": id,
+                    "vector": vector,
+                    "payload": metadata
+                }
+            ]
+        )
     
     def search(self, vector:list[float], top_k:int)->list[dict]:
-        pass
+        search_result = self.client.search(
+            collection_name=self.collection_name,
+            vector=vector,
+            limit=top_k
+        )
+        ids=[hit.id for hit in search_result]
+        counts = Counter(ids)
+        total_count = len(ids)
+        for item_id, count in counts.most_common():
+             percentage = (count / total_count) * 100
+        return zip(search_result, percentage)
