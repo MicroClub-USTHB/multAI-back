d17a05c2186e0cd9f9ce5a6013552f30bff98139
ademboukabes <adamboukabes1@gmail.com>
2026-03-15 14:25:48 +0100

    fix staff password migration and merge alembic heads

--- migrations/sql/up/replace-staff-discord-with-password.sql
+++ migrations/sql/up/replace-staff-discord-with-password.sql
@@ -1,2 +1,11 @@
-ALTER TABLE public.staff_users DROP COLUMN discord_id;
-ALTER TABLE public.staff_users ADD COLUMN password VARCHAR(255) NOT NULL ;
+ALTER TABLE public.staff_users ADD COLUMN password VARCHAR(255);
+
+UPDATE public.staff_users
+SET password = '$2b$12$ryQOrgtbfma5pQjN8/J2Q.D2sb4NUytTuiCr6YvKk/mDhqIWp9ZPO'
+WHERE password IS NULL;
+
+ALTER TABLE public.staff_users
+ALTER COLUMN password SET NOT NULL;
+
+ALTER TABLE public.staff_users
+DROP COLUMN discord_id;
