8a72509a7880a488bb17932dbb4d4a3601cca048
ademboukabes <adamboukabes1@gmail.com>
2026-03-15 14:25:48 +0100

    fix staff password migration and merge alembic heads

---
+++ migrations/versions/5ead72a95638_merge_alembic_heads.py
@@ -0,0 +1,28 @@
+"""merge alembic heads
+
+Revision ID: 5ead72a95638
+Revises: a4b8c2d9e3f1, b2e532644368
+Create Date: 2026-03-15 14:24:04.545981
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '5ead72a95638'
+down_revision: Union[str, Sequence[str], None] = ('a4b8c2d9e3f1', 'b2e532644368')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
