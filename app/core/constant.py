87a1536bef5ea7e7a4e7b66ae3c45e17117616fe
bouhamza abderrahmane <bouhamzaabouda@gmail.com>
2026-03-15 03:58:44 +0100

    Add image and enrollment constants for face enrollment validation

--- app/core/constant.py
+++ app/core/constant.py
@@ -4,3 +4,14 @@ from enum import Enum
 class RedisKey(str, Enum):
     UserSession = "user_session"
     UserSessionByUser = "user_session:{user_id}"
+
+IMAGE_ALLOWED_TYPES = {
+    "image/jpeg",
+    "image/png",
+    "image/heic",
+    "image/heif"
+}
+
+MAX_IMAGE_SIZE = 5 * 1024 * 1024
+MIN_ENROLL_IMAGES = 3
+MAX_ENROLL_IMAGES = 5
