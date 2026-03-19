a968c43c2504a7ffe2dfbc5ef22cbd8c2fa7c77d
wailbentafat <wailbentafat@gmail.com>
2026-03-19 20:29:06 +0100

    s7a 3idkom

--- app/router/mobile/__init__.py
+++ app/router/mobile/__init__.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
 from app.router.mobile.auth import router as mobile_auth_router
+from app.router.mobile.enrollement import router as onboarding_router
+from app.router.mobile.event import router as event_router
+
 
 router = APIRouter(prefix="/user",tags=["user"])
 router.include_router(mobile_auth_router)
+router.include_router(onboarding_router)
+router.include_router(event_router)
