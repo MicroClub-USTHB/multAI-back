8a054fde2fb6e229960aced5deacd3ab89ebb94d
wailbentafat <wailbentafat@gmail.com>
2026-03-19 20:02:04 +0100

    feat:add me endpoint

--- app/router/web/auth.py
+++ app/router/web/auth.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Depends
 from app.container import Container, get_container
 from fastapi import Response
 
+from app.deps.staff_auth import get_current_staff_user
 from app.schema.request.web.auth import WebAuthRequest
 from app.schema.response.web.auth import WebAuthResponse
+from db.generated.models import StaffUser
 router = APIRouter(prefix="/auth")
 
 @router.post("/login", response_model=WebAuthResponse,description="so here both the dahbsoard will authneticate from this endpoitn ")
 async def admin_login(
-    
     req: WebAuthRequest,
     r:Response,
     container: Container = Depends(get_container),
@@ -27,9 +28,11 @@ async def admin_login(
         max_age=60 * 60 * 24 * 7,
     )
     return authResponse
+
+
 @router.get("/me",response_model=WebAuthResponse)
 async def get_me_admin(
-    
+    user:StaffUser = Depends(get_current_staff_user),
     container:Container = Depends(get_container)
 ):
-    pass
+    return user
