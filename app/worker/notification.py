bb040454113483233be1754cb9b3d3c445f23e53
wailbentafat <wailbentafat@gmail.com>
2026-03-20 02:25:33 +0100

    feat: Introduce new services for auditing and user notifications, and workers for notification management and storage cleaning.

---
+++ app/worker/notification.py
@@ -0,0 +1,13 @@
+async def initilize_notifcation_services():
+        pass
+async def init_fcm():
+    pass
+async def init_apn():
+    pass
+async def init_web_push():
+    pass
+
+async def init_container():
+    # we use container here to intilzie connection with asyncpg and then we get device_id using the user_id 
+async def  listen_nats_event():
+    pass
