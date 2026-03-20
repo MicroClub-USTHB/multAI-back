0ab79e69be5a09bc5864797be9e2e5de58409948
maya-ots <mimiotsmane3@gmail.com>
2026-03-20 01:11:41 +0100

    fix : remove load_model

---
+++ app/service/AIService/models/face_detection.py
@@ -0,0 +1,31 @@
+from insightface.app import FaceAnalysis
+from typing import List, Tuple
+
+
+class FaceDetection:
+    def __init__(self, model_name: str = "buffalo_l"):
+        self.model = model_name
+        self.app = None
+    
+    def load_model(self):
+        self.app = FaceAnalysis(name=self.model, allowed_modules=['detection'])
+        print("[FaceDetection] model loaded successfully!")
+    
+    
+    def init_model(self):
+        if self.app is None:
+            raise ValueError("model not loaded. call load_model() first.")
+        self.app.prepare(ctx_id=0, det_size=(640, 640))  # ctx_id=0 → GPU, -1 for CPU
+        print("[INFO] Model initialized and ready.")
+    
+    def detect(self, image)->list[tuple[int,int,int,int]]:
+        if self.app is None:
+            raise ValueError("model not loaded. call load_model() and init_model() first.")
+       
+        faces = self.app.get(image)
+        bboxes: List[Tuple[int, int, int, int]] = []
+        for face in faces:
+            x1, y1, x2, y2 = face.bbox.astype(int)
+            bboxes.append((x1, y1, x2, y2))
+        return bboxes
+    
