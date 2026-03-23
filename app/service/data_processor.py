e5fbf0eaf4539574df3fda5156ba8f9ae317f601
merakleee <mahammedabdallahslimane@gmail.com>
2026-03-23 13:40:48 +0100

    Add type hints to BaseFilter methods

--- app/service/data_processor.py
+++ app/service/data_processor.py
@@ -1,19 +1,20 @@
 from abc import ABC, abstractmethod
+import numpy as np
 
 
 class BaseFilter(ABC):
 
     @abstractmethod
-    def verify_image(self, image) -> bool:
+    def verify_image(self, image: np.ndarray) -> bool:
         pass
 
     @abstractmethod
-    def process_image(self, image):
+    def process_image(self, image: np.ndarray) -> np.ndarray:
         pass
 
 
 class FilterFactory:
-    def __init__(self):
+    def __init__(self) -> None:
         from app.service.filters import BlurFilter, BrightnessFilter
 
         self.filters = {
