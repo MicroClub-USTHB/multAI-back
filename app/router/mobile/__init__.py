3cc2253bef02564d9f9dd53e1e14f15c4d8b7ccd
wailbentafat <wailbentafat@gmail.com>
2026-03-25 15:33:20 +0100

    Add audit gateway and notification service

--- app/router/mobile/__init__.py
+++ app/router/mobile/__init__.py
@@ -3,6 +3,7 @@ from app.router.mobile.auth import router as mobile_auth_router
 from app.router.mobile.enrollement import router as onboarding_router
 from app.router.mobile.event import router as event_router
 from app.router.mobile.notifications import router as mobile_notifications_router
+from app.router.mobile.audit import router as audit_router
 
 
 router = APIRouter(prefix="/user", tags=["user"])
@@ -11,3 +12,4 @@ router.include_router(mobile_auth_router)
 router.include_router(onboarding_router)
 router.include_router(event_router)
 router.include_router(mobile_notifications_router)
+router.include_router(audit_router)
